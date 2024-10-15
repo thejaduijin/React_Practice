d0458456ed66c4435190687751a877e223eaaf39
Lokendra Singh <lokendra.06.work@gmail.com>
2024-10-15 15:50:59 +0530

    useReducer example

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react'
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increase":
+      return state + 10;
+    case "decrease":
+      if (state === 150) {
+        return state;
+      } else {
+        return state - 10;
+      }
+    default:
+      return state;
+  }
+};
+
+
+function Button() {
+  const [width, dispatch] = useReducer(reducer, 500);
+  // const increaseWidth = () => dispatch({ type: "increase" });
+  const decreaseWidth = () => dispatch({ type: "decrease" });
+
+  return (
+    <div>
+      <h1>Click the button to see it grow:</h1>
+      <button style={{ width }} onClick={decreaseWidth}>
+        I Do Something
+      </button>
+    </div>
+  )
+}
+
+export default Button
--- src/components/Practice/Practice.jsx
+++ src/components/Practice/Practice.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./Practice.css"
 import Stopwatch from '../Stopwatch/Stopwatch';
+import Button from '../Button/Button';
 
 function Practice(props) {
     return (
         <div className='main-outlet'>
             Practice Component of {props.tech}
             <p className='heading'>Let's Start</p>
-            <Stopwatch></Stopwatch>
+            <Stopwatch></Stopwatch> "to see the example of useEffect"
+            <Button /> "to see the example of useReducer"
         </div>
     )
 }
